5d0468aa57cdec746e24f32bee0278ee96f21183
moving7 <1115123081@qq.com>
2017-12-07 02:05:25 +0800

    完善框架

--- core/common/function.php
+++ core/common/function.php
@@ -1,4 +1,7 @@
 <?php
+
+use core\lib\conf;
+
 function p($var)
 {
     if(is_bool($var)) {
@@ -7,5 +10,174 @@ function p($var)
         var_dump(NULL);
     } else {
         echo "<pre style='position:relative;z-index:1000;padding:10px;border-radius:5px;background:#f5f5f5;border:1px solid #aaa;font-size:14px;line-height:18px;opacity:0.9;'>".print_r($var, true)."</pre>";
+    }
+}
+
+/**
+ * 获取输入参数 支持过滤和默认值
+ * 使用方法:
+ * <code>
+ * I('id',0); 获取id参数 自动判断get或者post
+ * I('post.name','','htmlspecialchars'); 获取$_POST['name']
+ * I('get.'); 获取$_GET
+ * </code>
+ * @param string $name 变量的名称 支持指定类型
+ * @param mixed $default 不存在的时候默认值
+ * @param mixed $filter 参数过滤方法
+ * @param mixed $datas 要获取的额外数据源
+ * @return mixed
+ */
+function Input($name,$default='',$filter=null,$datas=null) {
+    static $_PUT	=	null;
+    if(strpos($name,'/')){ // 指定修饰符
+        list($name,$type) 	=	explode('/',$name,2);
+    }elseif(conf::get_conf('VAR_AUTO_STRING','conf')){ // 默认强制转换为字符串
+        $type   =   's';
+    }
+    if(strpos($name,'.')) { // 指定参数来源
+        list($method,$name) =   explode('.',$name,2);
+    }else{ // 默认为自动判断
+        $method =   'param';
+    }
+    switch(strtolower($method)) {
+        case 'get'     :
+            $input =& $_GET;
+            break;
+        case 'post'    :
+            $input =& $_POST;
+            break;
+        case 'put'     :
+            if(is_null($_PUT)){
+                parse_str(file_get_contents('php://input'), $_PUT);
+            }
+            $input 	=	$_PUT;
+            break;
+        case 'param'   :
+            switch($_SERVER['REQUEST_METHOD']) {
+                case 'POST':
+                    $input  =  $_POST;
+                    break;
+                case 'PUT':
+                    if(is_null($_PUT)){
+                        parse_str(file_get_contents('php://input'), $_PUT);
+                    }
+                    $input 	=	$_PUT;
+                    break;
+                default:
+                    $input  =  $_GET;
+            }
+            break;
+        case 'path'    :
+            $input  =   array();
+            if(!empty($_SERVER['PATH_INFO'])){
+                $depr   =   conf::get_conf('URL_PATHINFO_DEPR','conf');
+                $input  =   explode($depr,trim($_SERVER['PATH_INFO'],$depr));
+            }
+            break;
+        case 'request' :
+            $input =& $_REQUEST;
+            break;
+        case 'session' :
+            $input =& $_SESSION;
+            break;
+        case 'cookie'  :
+            $input =& $_COOKIE;
+            break;
+        case 'server'  :
+            $input =& $_SERVER;
+            break;
+        case 'globals' :
+            $input =& $GLOBALS;
+            break;
+        case 'data'    :
+            $input =& $datas;
+            break;
+        default:
+            return null;
+    }
+    if(''==$name) { // 获取全部变量
+        $data       =   $input;
+        $filters    =   isset($filter)?$filter:conf::get_conf('DEFAULT_FILTER','conf');
+        if($filters) {
+            if(is_string($filters)){
+                $filters    =   explode(',',$filters);
+            }
+            foreach($filters as $filter){
+                $data   =   array_map_recursive($filter,$data); // 参数过滤
+            }
+        }
+    }elseif(isset($input[$name])) { // 取值操作
+        $data       =   $input[$name];
+        $filters    =   isset($filter)?$filter:conf::get_conf('DEFAULT_FILTER','conf');
+        if($filters) {
+            if(is_string($filters)){
+                if(0 === strpos($filters,'/')){
+                    if(1 !== preg_match($filters,(string)$data)){
+                        // 支持正则验证
+                        return   isset($default) ? $default : null;
+                    }
+                }else{
+                    $filters    =   explode(',',$filters);
+                }
+            }elseif(is_int($filters)){
+                $filters    =   array($filters);
+            }
+
+            if(is_array($filters)){
+                foreach($filters as $filter){
+                    if(function_exists($filter)) {
+                        $data   =   is_array($data) ? array_map_recursive($filter,$data) : $filter($data); // 参数过滤
+                    }else{
+                        $data   =   filter_var($data,is_int($filter) ? $filter : filter_id($filter));
+                        if(false === $data) {
+                            return   isset($default) ? $default : null;
+                        }
+                    }
+                }
+            }
+        }
+        if(!empty($type)){
+            switch(strtolower($type)){
+                case 'a':	// 数组
+                    $data 	=	(array)$data;
+                    break;
+                case 'd':	// 数字
+                    $data 	=	(int)$data;
+                    break;
+                case 'f':	// 浮点
+                    $data 	=	(float)$data;
+                    break;
+                case 'b':	// 布尔
+                    $data 	=	(boolean)$data;
+                    break;
+                case 's':   // 字符串
+                default:
+                    $data   =   (string)$data;
+            }
+        }
+    }else{ // 变量默认值
+        $data       =    isset($default)?$default:null;
+    }
+    is_array($data) && array_walk_recursive($data,'wei_filter');
+    return $data;
+}
+
+function array_map_recursive($filter, $data) {
+    $result = array();
+    foreach ($data as $key => $val) {
+        $result[$key] = is_array($val)
+            ? array_map_recursive($filter, $val)
+            : call_user_func($filter, $val);
+    }
+    return $result;
+}
+
+// TODO 其他安全过滤
+function wei_filter(&$value)
+{
+
+    // 过滤查询特殊字符
+    if (preg_match('/^(EXP|NEQ|GT|EGT|LT|ELT|OR|XOR|LIKE|NOTLIKE|NOT BETWEEN|NOTBETWEEN|BETWEEN|NOTIN|NOT IN|IN)$/i', $value)) {
+        $value .= ' ';
     }
 }
---
+++ core/config/conf.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * 相关配置
+ */
+return [
+    /*默认参数转字符串*/
+    'VAR_AUTO_STRING' => false,
+    /*PathInfo分割方式*/
+    'URL_PATHINFO_DEPR' => '/',
+    /*参数过滤方式*/
+    'DEFAULT_FILTER' => 'htmlspecialchars'
+];
