608b2da85c41e5da4ea991ec126e85e9a5063589
moving7 <1115123081@qq.com>
2017-12-07 09:27:37 +0800

    完善参数小部件

---
+++ core/config/conf.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * 配置文件
+ */
+
+return [
+    /*默认参数转字符串*/
+    'VAR_AUTO_STRING' => false,
+    /*PathInfo分割方式*/
+    'URL_PATHINFO_DEPR' => '/',
+    /*参数过滤方式*/
+    'DEFAULT_FILTER' => 'htmlspecialchars',
+    /*默认时区*/
+    'TIMEZONE' => 'PRC',
+];
